e72a6881795e7afd5db137a9f1bf58a8c9dc6316
BrunoNC22 <thebegone2@gmail.com>
2024-08-06 22:54:13 -0300

    feat: add CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  app.enableCors({
+    origin: configService.get<string>('CORS_ORIGIN'),
+  })
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()

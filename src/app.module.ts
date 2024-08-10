4f107eb0a851bb82c7fc779a756ad7aefd7c9c93
BrunoNC22 <thebegone2@gmail.com>
2024-08-10 18:57:14 -0300

    feat: add integration with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,43 @@
 import { Module, ValidationPipe } from '@nestjs/common'
 import { JobOpportunityController } from './application/controllers/JobOpportunity.controller'
 import { JobOpportunityService } from './application/services/JobOpportiunity.service'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { APP_PIPE } from '@nestjs/core'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { DataSource } from 'typeorm'
+import { Company } from './infra/entities/Company.entity'
+import { CompanyService } from './application/services/Company.service'
+import { CompanyController } from './application/controllers/Company.controller'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts}'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([Company]),
   ],
-  controllers: [JobOpportunityController],
+  controllers: [JobOpportunityController, CompanyController],
   providers: [
     JobOpportunityService,
+    CompanyService,
     { provide: APP_PIPE, useClass: ValidationPipe },
   ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}

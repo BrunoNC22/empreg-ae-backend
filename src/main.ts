1e09b902fdb417152a2937ba1fe96eeee801754c
BrunoNC22 <thebegone2@gmail.com>
2024-08-11 04:00:42 -0300

    feat: create swagger boilerplate

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -9,6 +10,15 @@ async function bootstrap() {
     origin: configService.get<string>('CORS_ORIGIN'),
   })
   const app_port = configService.get<string>('APP_PORT')
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('EmpregAÊ api')
+    .setDescription('Documentação da api do EmpregAÊ!')
+    .setVersion('0.1')
+    .build()
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('documentation', app, swaggerDocument)
+
   await app.listen(app_port ? app_port : 3000)
 }
 bootstrap()

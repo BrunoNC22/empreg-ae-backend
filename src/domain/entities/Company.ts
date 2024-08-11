cbf76e7177c22b3bdc10daf683415a2ab31a4bf2
BrunoNC22 <thebegone2@gmail.com>
2024-08-11 04:09:04 -0300

    feat: add getters and setters to Company.ts

--- src/domain/entities/Company.ts
+++ src/domain/entities/Company.ts
@@ -1,6 +1,18 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export class Company {
-  constructor(
-    private id: string,
-    private companyName: string,
-  ) {}
+  private _id: string
+  constructor(private _companyName: string) {
+    this._id = uuidv4()
+  }
+
+  public get companyName(): string {
+    return this._companyName
+  }
+  public set companyName(value: string) {
+    this._companyName = value
+  }
+  public get id(): string {
+    return this._id
+  }
 }

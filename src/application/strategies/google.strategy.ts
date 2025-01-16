fe54e2c290cd54c99f9dfb9556d823be60434cba
BrunoNC22 <thebegone2@gmail.com>
2025-01-16 18:02:37 -0300

    feat: create google oauth strategy

---
+++ src/application/strategies/google.strategy.ts
@@ -0,0 +1,38 @@
+import { Inject, Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { Strategy, VerifyCallback } from 'passport-google-oauth20'
+import googleOauthConfig from '../config/google-oauth.config'
+import { ConfigType } from '@nestjs/config'
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(
+    @Inject(googleOauthConfig.KEY)
+    private googleConfiguration: ConfigType<typeof googleOauthConfig>,
+  ) {
+    super({
+      clientID: googleConfiguration.clientId,
+      clientSecret: googleConfiguration.clientSecret,
+      callbackURL: googleConfiguration.callbackURL,
+      scope: ['email', 'profile'],
+    })
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ) {
+    const { name, emails, photos } = profile
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+      refreshToken,
+    }
+    done(null, user)
+  }
+}
